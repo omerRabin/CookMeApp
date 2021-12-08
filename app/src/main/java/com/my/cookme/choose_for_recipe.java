d6bfa05c75b04e76476dd1b0577592d4ca0c3f67
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-12-08 20:23:12 +0200

    Adding Search for A recipe

--- app/src/main/java/com/my/cookme/choose_for_recipe.java
+++ app/src/main/java/com/my/cookme/choose_for_recipe.java
@@ -1,39 +1,102 @@
 package com.my.cookme;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class choose_for_recipe extends AppCompatActivity {
 
-    AutoCompleteTextView suggestion_box;
-    Spinner items;
+    ListView myListView;
+    Spinner mySpinner;
+    ArrayAdapter<CosmicBody> adapter;
+    String[] categories = {"Vegetables&Fruits","Meat","Dairy Products", "Spices", "Cereals and Legums","Fish" };
 
+    private void initializeViews() {
+        mySpinner = findViewById(R.id.mySpinner);
+        mySpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,categories));
+        myListView = findViewById(R.id.myListView);
+        myListView.setAdapter(new ArrayAdapter<>(choose_for_recipe.this, android.R.layout.simple_list_item_1,getCosmicBodies()));
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position >= 0 && position <categories.length) {
+                    getSelectedCategoryData(position);
+                }
+                else{
+                    Toast.makeText( choose_for_recipe.this, "selected category doesnt exist!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private ArrayList<CosmicBody> getCosmicBodies() {
+        ArrayList<CosmicBody> data = new ArrayList<>();
+        data.clear(); // here i need to put the all ingredients from data that i need to pull from fire base
+        data.add(new CosmicBody("Dairy Products", 1));
+        return data;
+    }
+    private void getSelectedCategoryData(int categoryID) {
+        ArrayList<CosmicBody> cosmicBodies = new ArrayList<>();
+        if(categoryID ==0)
+        {
+            adapter = new ArrayAdapter<>(choose_for_recipe.this, android.R.layout.simple_list_item_1, getCosmicBodies());
+        }
+        else
+        {
+            for(CosmicBody cosmicBody : getCosmicBodies()) {
+                if(cosmicBody.getCategoryID() == categoryID) {
+                    cosmicBodies.add(cosmicBody);
+                }
+            }
+            adapter = new ArrayAdapter<>(choose_for_recipe.this, android.R.layout.simple_list_item_1,cosmicBodies);
+        }
+        myListView.setAdapter(adapter);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_for_recipe);
-        ArrayList<String> foods = new ArrayList<>();
-        suggestion_box = (AutoCompleteTextView)findViewById(R.id.suggestion_box);
-        items = (Spinner) findViewById(R.id.actionDownUp);
 
-        foods.add("apple");
-        foods.add("banana");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(choose_for_recipe.this, android.R.layout.simple_spinner_dropdown_item,foods);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(choose_for_recipe.this, android.R.layout.simple_spinner_dropdown_item,foods);
-        suggestion_box.setAdapter(adapter);
-        items.setAdapter(adapter1);
+        initializeViews();
+    }
+}
 
+class CosmicBody {
+    private String name;
+    private int categoryID;
+
+    public String getName() {
+        return name;
     }
 
+    public int getCategoryID() {
+        return categoryID;
+    }
 
-    public void show_vegetables_fruits(View view){
+    public CosmicBody(String name, int categoryID) {
+        this.name = name;
+        this.categoryID = categoryID;
+    }
+
+    @Override
+    public String toString(){
+        return name;
     }
 
 }
