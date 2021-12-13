6d56edf6554a8cc4e56f9adda727de489558ec48
Yoel Hartman <yoel2810@gmail.com>
2021-12-13 15:09:00 +0200

    Set up the updated ingredients

--- app/src/main/java/com/my/cookme/UploadIngredientActivity.java
+++ app/src/main/java/com/my/cookme/UploadIngredientActivity.java
@@ -1,21 +1,32 @@
 package com.my.cookme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class UploadIngredientActivity extends AppCompatActivity {
     
     private EditText editTextName;
     private EditText editTextCategory;
     private Button buttonAdd;
+    private TextView textViewShowUpdates;
+
+    private DatabaseReference updateDBRef;
     private DatabaseReference ingredientDBRef;
 
     @Override
@@ -25,6 +36,7 @@ public class UploadIngredientActivity extends AppCompatActivity {
         
         this.editTextName = findViewById(R.id.editTextIngredientName);
         this.editTextCategory = findViewById(R.id.editTextCategory);
+        this.updateDBRef = FirebaseDatabase.getInstance().getReference().child("Update");
         this.ingredientDBRef = FirebaseDatabase.getInstance().getReference().child("Ingredients");
         this.buttonAdd = findViewById(R.id.ButtonAddIngredient);
         
@@ -34,6 +46,8 @@ public class UploadIngredientActivity extends AppCompatActivity {
                 insertIngredientData();
             }
         });
+
+        showNeededUpdates();
     }
 
     private void insertIngredientData() {
@@ -42,5 +56,26 @@ public class UploadIngredientActivity extends AppCompatActivity {
         Ingredient i = new Ingredient(name, null, category);
         ingredientDBRef.push().setValue(i);
         Toast.makeText(UploadIngredientActivity.this, "UPLOADED", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showNeededUpdates() {
+        this.textViewShowUpdates = findViewById(R.id.showIngredientsTxt);
+        updateDBRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    String content = "";
+                    HashMap<String, String> objectHashMap = (HashMap<String, String>) task.getResult().getValue();
+                    for (String obj : objectHashMap.values()) {
+                        content += obj + "\n";
+                    }
+                    textViewShowUpdates.setText(content);
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
     }
 }
