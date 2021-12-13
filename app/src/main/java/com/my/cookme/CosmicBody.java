54c8bca7413907a6ca92576157745fef4b3aa559
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-12-13 14:27:06 +0200

    update list of ingredients for each category

---
+++ app/src/main/java/com/my/cookme/CosmicBody.java
@@ -0,0 +1,43 @@
+package com.my.cookme;
+public class CosmicBody {
+    private String name;
+    private String category;
+    private int categoryID;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public CosmicBody(String name, String category) {
+        this.name = name;
+        if(category.equals("Vegetables&Fruits"))
+        {
+            this.categoryID = 1;
+        }
+        else if(category.equals("Meat")){
+            this.categoryID = 2;
+        }
+        else if(category.equals("Dairy Products")){
+            this.categoryID =3;
+        }
+        else if(category.equals("Spices")){
+            this.categoryID =4;
+        }
+        else if (category.equals("Cereals and Legums")){
+            this.categoryID =5;
+        }
+        else if(category.equals("Fish")){
+            this.categoryID =6;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+}
