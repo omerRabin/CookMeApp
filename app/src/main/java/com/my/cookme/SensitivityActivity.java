ba6851e702ad68f6c0e56b664c05a269633e42b3
yoel2810 <yoel2810@gmail.com>
2021-12-17 17:15:52 +0200

    Setting up the sensitivity activity.

--- app/src/main/java/com/my/cookme/SensitivityActivity.java
+++ app/src/main/java/com/my/cookme/SensitivityActivity.java
@@ -3,8 +3,15 @@ package com.my.cookme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SensitivityActivity extends AppCompatActivity {
+
+    private EditText sen;
+    private Button addSensitivity;
+    private Button removeSensitivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,16 @@ public class SensitivityActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sensitivity);
         getSupportActionBar().setTitle("CookMe");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.sen = findViewById(R.id.editTextIngredientSensitive);
+        this.addSensitivity = findViewById(R.id.buttonAddSensitivity);
+        this.removeSensitivity = findViewById(R.id.buttonRemoveSensitivity);
+
+        this.addSensitivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
